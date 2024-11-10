c70d6ca574d3fe0c8b8f93af5fc65cfa674e5d8f
Alex Zima <xzima@ro.ru>
2024-11-10 03:14:13 +0300

    chore: install husky git-hooks and commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    // https://commitlint.js.org/#/reference-rules
+    rules: {
+        'body-max-line-length': [2, 'always', 150],
+    },
+    ignores: [(commit) => ['[skip ci]', '[skip ci]', '[no ci]'].some(it => commit.includes(it)) ||
+        /Create PR for #\d+/.test(commit)],
+};
