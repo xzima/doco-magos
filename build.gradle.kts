29ffd1f699b33a456b3144d5d9ae153cbd116de1
Alex Zima <xzima@ro.ru>
2024-12-16 01:30:30 +0300

    feat: configure spotless plugin for generate license headers
    - clear gradle.properties
    - add and configure licenseHeader with spotless plugin
    - add link to OCI-specified labels in note.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.diffplug.gradle.spotless.BaseKotlinExtension
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
@@ -9,6 +10,8 @@ plugins {
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    // applied for all subprojects
+    alias(libs.plugins.spotless)
 }
 
 rootProject.plugins.withType(YarnPlugin::class.java) {
@@ -17,3 +20,41 @@ rootProject.plugins.withType(YarnPlugin::class.java) {
         yarnLockAutoReplace = true
     }
 }
+
+spotless {
+    ratchetFrom = "origin/master"
+    val yearPlaceholder = "\$YEAR"
+    val author = "Alex Zima(xzima@ro.ru)"
+    kotlin {
+        target("**/*.kt")
+        configureSpotlessKotlin(yearPlaceholder, author)
+    }
+    kotlinGradle {
+        target("**/*.gradle.kts")
+        configureSpotlessKotlin(yearPlaceholder, author)
+    }
+}
+
+fun BaseKotlinExtension.configureSpotlessKotlin(yearPlaceholder: String, author: String) {
+    licenseHeader(
+        """
+        /**
+         * Copyright $yearPlaceholder $author
+         *
+         * Licensed under the Apache License, Version 2.0 (the "License");
+         * you may not use this file except in compliance with the License.
+         * You may obtain a copy of the License at
+         *
+         *     https://www.apache.org/licenses/LICENSE-2.0
+         *
+         * Unless required by applicable law or agreed to in writing, software
+         * distributed under the License is distributed on an "AS IS" BASIS,
+         * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+         * See the License for the specific language governing permissions and
+         * limitations under the License.
+         */
+        """.trimIndent(),
+        "(package |@file|import |plugins )", // based on KotlinConstants.LICENSE_HEADER_DELIMITER
+    )
+}
+
